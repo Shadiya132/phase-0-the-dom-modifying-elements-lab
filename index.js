6886598e7d4638a6beb70271257a68639f20a507
Shadiya Egal <shadiya325@gmail.com>
2022-11-14 04:20:54 -0500

    commit tree 0a96faf2be8501b92247f7751f47d7916f0afdd2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove(main);
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory", "");
+newHeader.textContent = "Shadiya is the champion"
